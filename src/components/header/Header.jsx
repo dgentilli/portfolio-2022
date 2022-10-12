0bf153e001630d903eda43f34f6bee7e8f7d48ab
Dave Gentilli <dgentilli@gmail.com>
2022-10-12 09:45:21 +0300

    Split the navbar into a separate component and reuse colors throughout the entire app.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Header.module.css';
+import Navbar from '../navbar/Navbar';
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <nav className={styles.nav_bar}></nav>
+      <Navbar />
       <div className={styles.header_text_wrapper}>
         <h1 className={styles.header_title}>Dave Gentilli</h1>
         <h3 className={styles.header_sub_title}>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <div className={styles.nav_bar}>
+      <div className={styles.nav_bar_home}>Home</div>
+    </div>
+  );
+};
+
+export default Navbar;
