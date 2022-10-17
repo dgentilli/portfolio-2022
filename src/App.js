61cd0bcf7e27432a64b2261b6ee51d0df2e77df7
Dave Gentilli <dgentilli@gmail.com>
2022-10-17 21:52:54 +0300

    Reorganize code for better clarity / readability.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <Header />
     </div>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import styles from './Header.module.css';
-import Navbar from '../navbar/Navbar';
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <Navbar />
       <div className={styles.header_text_wrapper}>
         <h1 className={styles.header_title}>Dave Gentilli</h1>
         <h3 className={styles.header_sub_title}>
