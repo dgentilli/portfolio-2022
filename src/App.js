aa10faad298ef1d17464272df88ab251f98e326b
Dave Gentilli <dgentilli@gmail.com>
2022-10-21 21:49:03 +0300

    Add a section and some cards to the site.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
+import Section from './components/section/Section';
+import Card from './components/card/Card';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <Header />
+      <Section title='Client Projects' modifierClass='section__dark'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Section>
     </div>
   );
 }
