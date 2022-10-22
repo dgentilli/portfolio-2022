931fed2087516a887f1b19f876ececb119c562e7
Dave Gentilli <dgentilli@gmail.com>
2022-10-22 11:30:17 +0300

    Add a data file for experience.

---
+++ src/data/experienceData.js
@@ -0,0 +1,14 @@
+export const experienceData = {
+  title: 'Professional Developer Since 2020',
+  coreTechSkills: 'React Native, React, JavaScript',
+  softSkills:
+    'Critical thinking, teamwork, planning and organizing, managing client relationships',
+  paragraphs: {
+    overview:
+      'Prior to becoming a developer, I developed a broad range of transferrable skills such as critical thinking, structured analysis, teamwork, planning and organizing, and working as part of multi-cultural teams. I cultivated these skills over more than 15 years during which I served as a U.S. Army officer, worked as an analyst, and worked as a civilian contractor with the U.S. Department of Defense and Department of the Army.',
+    military:
+      'I planned and organized a training deployment of all soldiers, weapons and sensitive items, and equipment totaling over $25 Million as the second-in-command of an infantry company. I also planned and organized a change of command inventory for all of the above equipment. Oversaw maintenance operations and planned the logistics and resources for training exercises. ',
+    contractor:
+      'As a contractor I excelled at forging relationships with client organizations in every role I held. This success rested upon my communication skills, most importantly listening skills, so I could understand each client organization’s needs and tailor my work accordingly. ',
+  },
+};
