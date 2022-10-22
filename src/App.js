d5d504fab3eb8921d3de68d8d87d04cc88c5640a
Dave Gentilli <dgentilli@gmail.com>
2022-10-22 11:32:08 +0300

    Create an unstyled experience list with hard coded data as a POC.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
 import Section from './components/section/Section';
 import Card from './components/card/Card';
+import ExperienceList from './components/experienceList/ExperienceList';
 
 import { cardData } from './data/cardData';
+import { sectionTypes } from './enums';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <Header />
-      <Section title='Client Projects' sectionType='light'>
+      <Section title='Client Projects' sectionType={sectionTypes.LIGHT}>
         {cardData.map((item) => (
           <Card
             cardTitle={item.cardTitle}
@@ -19,6 +21,9 @@ function App() {
             description={item.description}
           />
         ))}
+      </Section>
+      <Section title='Previous Experience' sectionType={sectionTypes.MEDIUM}>
+        <ExperienceList />
       </Section>
     </div>
   );
---
+++ src/components/experienceList/ExperienceList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const ExperienceList = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <p>Professional developer since 2020</p>
+          <p>JavaScript, React, React Native</p>
+        </li>
+        <li>
+          <p>Contracting and consulting</p>
+          <p>
+            Developed experience in client service delivery and building client
+            relationships.
+          </p>
+        </li>
+        <li>
+          <p>U.S. Army officer</p>
+          <p>
+            Ton of experience planning, organizing, leading, and working as part
+            of a team.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ExperienceList;
