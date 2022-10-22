b68b93509ab1713d972cffa03403bfaae1c1d99d
Dave Gentilli <dgentilli@gmail.com>
2022-10-22 11:31:40 +0300

    Clean up code in the card module and increase line height.

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import styles from './Card.module.css';
 
-const Card = ({ cardTitle, job, period, description }) => {
-  return (
-    <div className={styles.card}>
-      <h5 className={styles.card_title}>{cardTitle}</h5>
-      <p className={styles.card_job}>{job}</p>
-      <p className={styles.card_period}>{period}</p>
-      <p className={styles.card_text}>{description}</p>
-    </div>
-  );
-};
+const Card = ({ cardTitle, job, period, description }) => (
+  <div className={styles.card}>
+    <h5 className={styles.card_title}>{cardTitle}</h5>
+    <p className={styles.card_job}>{job}</p>
+    <p className={styles.card_period}>{period}</p>
+    <p className={styles.card_text}>{description}</p>
+  </div>
+);
 
 export default Card;
