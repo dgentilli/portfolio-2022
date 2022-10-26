a29db68571a558b73bca12a9cc3197447687565f
Dave Gentilli <dgentilli@gmail.com>
2022-10-26 20:40:55 +0300

    Display svg icons for the background section.

--- src/components/backgroundDetails/Background.jsx
+++ src/components/backgroundDetails/Background.jsx
@@ -6,9 +6,18 @@ import { bioData } from '../../data/biodata';
 const Background = () => {
   return (
     <div className={styles.background_container}>
-      <p className={styles.background_paragraph}>{bioData.background}</p>
-      <p className={styles.background_paragraph}>{bioData.career}</p>
-      <p className={styles.background_paragraph}>{bioData.hobbies}</p>
+      <div>
+        <div className={styles.paragraph_icon_life} />
+        <p className={styles.background_paragraph}>{bioData.background}</p>
+      </div>
+      <div>
+        <div className={styles.paragraph_icon_career} />
+        <p className={styles.background_paragraph}>{bioData.career}</p>
+      </div>
+      <div>
+        <div className={styles.paragraph_icon_hobbies} />
+        <p className={styles.background_paragraph}>{bioData.hobbies}</p>
+      </div>
     </div>
   );
 };
