549286ae45675e8b729e6ddcbdfb181ca4838758
Dave Gentilli <dgentilli@gmail.com>
2022-10-29 19:08:04 +0300

    Update info text.

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -6,7 +6,7 @@ const Info = () => {
     <div className={styles.info_container}>
       <p className={styles.info_text}>
         For more info about please see my media accounts. Reach out to me on
-        LinkedIn
+        LinkedIn or at dgentilli@gmail.com.
       </p>
       <div className={styles.info_icon_container}>
         <div className={styles.info_linkedIn} />
