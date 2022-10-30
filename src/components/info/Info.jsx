da13d1ff172a6004d45d5402a985963f4e647d3f
Dave Gentilli <dgentilli@gmail.com>
2022-10-30 09:47:48 +0200

    Link to the social media accounts.

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -9,9 +9,18 @@ const Info = () => {
         LinkedIn or at dgentilli@gmail.com.
       </p>
       <div className={styles.info_icon_container}>
-        <div className={styles.info_linkedIn} />
-        <div className={styles.info_gitHub} />
-        <div className={styles.info_twitter} />
+        <a
+          className={styles.info_linkedIn}
+          href='https://ro.linkedin.com/in/dave-gentilli-97925892'
+        ></a>
+        <a
+          className={styles.info_gitHub}
+          href='https://github.com/dgentilli'
+        ></a>
+        <a
+          className={styles.info_twitter}
+          href='https://twitter.com/dave_gentilli'
+        ></a>
       </div>
     </div>
   );
