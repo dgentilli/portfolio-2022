c66a93725359764bbdaec785f6090c9b8afa6205
Dave Gentilli <dgentilli@gmail.com>
2022-11-02 19:41:39 +0200

    Implement smooth scrolling.

--- src/components/modal/MenuModal.jsx
+++ src/components/modal/MenuModal.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback } from 'react';
+import { Link } from 'react-scroll';
 import styles from './MenuModal.module.css';
 import { sectionTitles } from '../../enums';
 
@@ -7,9 +8,6 @@ const MenuModal = ({ setIsOpen }) => {
   const modalRef = React.useRef();
 
   const menuItemClickHandler = useCallback(() => {
-    if (modalRef?.current) {
-      modalRef.current.classList.add(styles.modal__isHidden);
-    }
     setIsOpen(false);
   }, [setIsOpen]);
 
@@ -19,15 +17,18 @@ const MenuModal = ({ setIsOpen }) => {
         X Close
       </div>
       <div className={styles.modal_text}>
-        {sectionArray.map((menuItem) => (
-          <a
+        {sectionArray.map((menuItem, index) => (
+          <Link
+            to={`${menuItem}`}
+            smooth={true}
+            offset={0}
+            duration={(index + 1) * 250}
             onClick={menuItemClickHandler}
-            href={`#${menuItem}`}
             className={styles.modal_item}
             key={menuItem}
           >
             {menuItem}
-          </a>
+          </Link>
         ))}
       </div>
     </div>
--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 import styles from './Footer.module.css';
+
+const LINK_TEXT = 'Back to top';
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <p>Copyright 2022</p>
-      <a href='#navbar'>Back to top</a>
+      <Link to={'navbar'} smooth={true} offset={0} duration={500}>
+        {LINK_TEXT}
+      </Link>
     </div>
   );
 };
